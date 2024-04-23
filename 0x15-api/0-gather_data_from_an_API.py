e55044e206248957ac9707172d32e0273dd36dd3
GODSPE1 <godspy18@gmail.com>
2024-04-23 03:56:59 +0100

    fetch data for a given employee ID, returns information about his/her TODO list progress

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+Fecting data for a given employee ID, returns information about
+his/her TODO list progress.
+"""
+import json
+import requests
+import sys
+
+
+def main():
+    # Checking if the correct number of command-line arguments is provided
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        return
+
+    # Getting the employee ID from the command-line argument
+    employee_id = sys.argv[1]
+
+    todos_response = requests.get(
+        f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
+    user_response = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{employee_id}')
+
+    todos = todos_response.json()
+    user = user_response.json()
+
+    user_tasks = {}
+
+    for todo in todos:
+        user_tasks[todo['id']] = {
+            'task': todo['title'],
+            'completed': todo['completed']
+        }
+
+    num_completed_tasks = sum(1 for todo in todos if todo['completed'])
+    total_tasks = len(todos)
+
+    print(f"Employee {user['name']} is done with tasks ({num_completed_tasks}/{total_tasks}):")
+    for todo in todos:
+        if todo['completed']:
+            print(f"\t{todo['title']}")
+
+    # Printing the user_tasks dictionary using json.dump()
+    print(json.dumps(user_tasks, indent=4))
+
+
+if __name__ == "__main__":
+    main()
