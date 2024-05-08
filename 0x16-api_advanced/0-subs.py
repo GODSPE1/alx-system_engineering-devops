5cad89c88fc95121a7835d2d6e1dac8f05085814
GODSPE1 <godspy18@gmail.com>
2024-05-08 02:51:52 +0100

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 """
-This script retrieves the number of subscribers for a given subreddit using the Reddit API.
+This script retrieves the number of subscribers for a
+given subreddit using the Reddit API.
 """
 
 import requests
 
-def number_of_subscribers(subreddit):
-    if not subreddit:
-        return 0
-    else:
-        url = f"https://www.reddit.com/r/{subreddit}/about.json"
-                
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            subscribers = data['data']['subscribers']
-            return subscribers
-        else:
-            return None
 
+def number_of_subscribers(subreddit):
+    """ returns the number of subscribers for a given subreddit"""
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    user_agent = {'User-agent': 'Go9'}
+    response = requests.get(url, allow_redirects=False, headers=user_agent)
+    data = response.json()
+    try:
+        return data.get('data').get('subscribers')
+    except Exception:
+        return 0
