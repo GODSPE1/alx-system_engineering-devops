3ad560c2bb883a8edb41be052d6bfd14edee5b9b
GODSPE1 <godspy18@gmail.com>
2024-05-08 02:08:24 +0100

    for ten hot reddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This script retrieves the number of hot posts on a given Redditg"""
+import requests
+
+
+def top_ten(subreddit):
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    
+    response = requests.get(url, allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
